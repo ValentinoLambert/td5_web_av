5b8268a124b1e9a089917366a0353006fa743301
e7160u <valenino.lambert94@gmail.com>
2026-01-08 15:09:03 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Application Vue</h1>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+</style>
