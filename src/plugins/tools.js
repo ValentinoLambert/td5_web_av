ce18dd674ae86f7312852aecd753a8c9f756c3ba
noe franoux <noe.franoux7@etu.univ-lorraine.fr>
2026-01-08 17:37:26 +0100

    merge

--- src/plugins/tools.js
+++ src/plugins/tools.js
@@ -3,9 +3,17 @@ export default {
     app.mixin({
       methods: {
         dateToDB(date) {
-          return date.split('/').reverse().join('-')
-        }
-      }
-    })
-  }
-}
+          return date.split("/").reverse().join("-");
+        },
+        dbDateToFr(dbDate) {
+          return dbDate.split("-").reverse().join("/");
+        },
+        dbDateHourToFr(dbDa) {
+          [date, hour] = dbDa.split(" ");
+          return this.dbDateToFr(date) + " اے " + hour;
+        },
+        
+      },
+    });
+  },
+};
