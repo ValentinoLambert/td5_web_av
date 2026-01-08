4a7adbf50d9a8191abc56c440893f9774dc5800e
e7160u <valenino.lambert94@gmail.com>
2026-01-08 17:32:45 +0100

    tools

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import apiPlugin from './plugins/api'
+import toolsPlugin from './plugins/tools'
 
 const app = createApp(App)
 
@@ -9,6 +10,9 @@ app.use(router)
 app.use(apiPlugin, {
   baseURL: 'https://donations.edu.netlog.fr',
   apiKey: import.meta.env.VITE_API_KEY
+})
+app.use(toolsPlugin, {
+  textMaxLength: 100
 })
 
 app.mount('#app')
--- src/plugins/tools.js
+++ src/plugins/tools.js
@@ -1,10 +1,30 @@
 export default {
-  install(app) {
+  install(app, options) {
     app.mixin({
       methods: {
         dateToDB(date) {
           return date.split('/').reverse().join('-')
+        },
+        dbDateToFr(date) {
+          return date.split('-').reverse().join('/')
+        },
+        dbDateHourToFr(datetime) {
+          const [date, time] = datetime.split(' ')
+          return this.dbDateToFr(date) + ' ' + time
+        },
+        formatAmount(amount) {
+          return amount.toLocaleString('fr-FR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ' €'
+        },
+        cutText(text, maxLength = options.textMaxLength) {
+          if (text.length <= maxLength) return text
+          return text.substring(0, maxLength) + '…'
         }
+      }
+    })
+
+    app.directive('focus', {
+      mounted(el) {
+        el.focus()
       }
     })
   }
